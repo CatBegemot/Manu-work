229e5193927897797065bd8aac35d3dbefb52daa
Volchar <volchar@gmail.com>
2019-08-11 22:09:55 +0300

    Make functions for main

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-var divImg = document.createElement("img");
+var divImg = document.createElement("div");
 document.body.appendChild(divImg);
-divImg.id = "pictMenu";
-divImg.style.position = "absolute";
-divImg.style.zIndex = -1;
-divImg.style.top = "10px"; 
-divImg.style.left = "10px";
-divImg.src = imgPath + "menuOn.png";
 
-divImg.id = "pictMain";
-divImg.style.position = "absolute";
-divImg.style.zIndex = 0;
-divImg.style.top = "45px"; 
-divImg.style.left = "10px";
-divImg.src = imgPath + "main.png";
+var imgMenu = document.createElement("img");
+divImg.appendChild(imgMenu);
+imgMenu.style.position = "absolute";
+imgMenu.style.zIndex = -1;
+imgMenu.style.top = "10px"; 
+imgMenu.style.left = "10px";
+imgMenu.src = imgPath + "menuOn.png";
 
-divImg.id = "pictSecond";
-divImg.style.position = "absolute";
-divImg.style.zIndex = 0;
-divImg.style.top = "80px"; 
-divImg.style.left = "10px";
-divImg.src = imgPath + "second.png";
+var imgMain = document.createElement("img");
+divImg.appendChild(imgMain);
+imgMain.style.position = "absolute";
+imgMain.style.zIndex = 1;
+imgMain.style.top = "45px"; 
+imgMain.style.left = "10px";
+imgMain.src = imgPath + "mainOn.png";
 
-divImg.id = "pictSecond";
-divImg.style.position = "absolute";
-divImg.style.zIndex = 0;
-divImg.style.top = "115px"; 
-divImg.style.left = "10px";
-divImg.src = imgPath + "third.png";
+var imgSecond = document.createElement("img");
+divImg.appendChild(imgSecond);
+imgSecond.style.position = "absolute";
+imgSecond.style.zIndex = 1;
+imgSecond.style.top = "80px"; 
+imgSecond.style.left = "10px";
+imgSecond.src = imgPath + "second.png";
+
+var imgThird = document.createElement("img");
+divImg.appendChild(imgThird);
+imgThird.style.position = "absolute";
+imgThird.style.zIndex = 1;
+imgThird.style.top = "115px"; 
+imgThird.style.left = "10px";
+imgThird.src = imgPath + "third.png";
 
 var linkMenu = document.createElement("A");
 pictMenu.onmouseover = function() {startMain()};
