8b1e5a8905278c00de768306e67c1dc8f5f99270
Volchar <volchar@gmail.com>
2019-08-16 00:47:48 +0300

    Change name of var

--- functionsViz_One.js
+++ functionsViz_One.js
@@ -1,11 +1,3 @@
-﻿function startHid() {
-  var viz = document.getElementById("imgHid");
-  if (viz.style.visibility === "hidden") {
-    viz.style.visibility = "visible";
-  } else {
-    viz.style.visibility = "hidden";
-  }
-}
-function changeSrc() {
-  Image.src = imgPath + "Black_pict.jpg";
+﻿function changeSrc() {
+    image.src = imgPath + "Black_pict.jpg";
  }
--- viz_hid_One.js
+++ viz_hid_One.js
@@ -1,15 +1,15 @@
 ﻿var divImg = document.createElement("div");
 document.body.appendChild(divImg);
 
-var Image = document.createElement("img");
-Image.id = "Image";
-divImg.appendChild(Image);
+var image = document.createElement("img");
+image.id = "image";
+divImg.appendChild(image);
   
-  Image.style.position = "absolute";
-  Image.style.zIndex = 1;
-  Image.style.top = "10px";
-  Image.style.left = "100px";
-  Image.src = imgPath + "Big_pict.jpg";
+  image.style.position = "absolute";
+  image.style.zIndex = 1;
+  image.style.top = "10px";
+  image.style.left = "100px";
+  image.src = imgPath + "Big_pict.jpg";
   
-  Image.onclick = function() {changeSrc()}; 
+  image.onclick = function() {changeSrc()}; 
   
