4b0e761e78bd29cd4a621fadb40c72d4fda27694
Volchar <volchar@gmail.com>
2019-08-22 20:31:11 +0300

    Made animation

--- functionsMenu.js
+++ functionsMenu.js
@@ -1,11 +1,21 @@
 function startViz() {
   var viz = document.getElementById("dropdown");
-    if (viz.style.visibility === "hidden") {
-        viz.style.visibility = "visible";} 
-    else {
-        viz.style.visibility = "hidden";
-          }
+  viz.style.visibility = "visible";
+  
+var elem = document.getElementById("dropdown");   
+  var pos = -160;
+  var id = setInterval(frame, 1);
+  function frame() {
+    if (pos == 0) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.left = pos + "px"; 
+      }
+    }
 }
+
+
 function startHid() {
   var viz = document.getElementById("dropdown");
   viz.style.visibility = "hidden";
